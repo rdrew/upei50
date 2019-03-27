c92bd5be63a93b4f4df802f90e64bfcf4d58f8fe
rdrew <robdrewupei@gmail.com>
2019-03-27 11:50:22 -0300

    revamped gulp to fix sourcemaps

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+//var cache = require('gulp-cache');
+
+
+//===================
+// Browsersync Proxy
+//===================
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "http://upei50.dev.islandarchives.ca/",
+        serveStatic: ['.'],
+        files: ['./dist/css/upei50.styles.css','./dist/js/upei50.behaviors.js'],
+        //files: ['./dist/css/upei50.styles.css'],
+        plugins: ['bs-rewrite-rules'],
+        rewriteRules: [
+            //{
+                //match: '/themes/custom/lmmi_journal/js/',
+                //replace: '/js/'
+            //},
+            {
+                match: '/themes/custom/lmmi_journal/css/',
+                replace: '/css/'
+            }
+        ]
+    });
+});
+
+
+//====================
+// Sass Compilation
+//===================
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass({
+            //includePaths: ['./node_modules/breakpoint-sass/stylesheets']
+        }))
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+//====================
+// JS Concatination
+//===================
+
+gulp.task('js', function () {
+    gulp.src('./src/js/*.js') // path to your files
+    .pipe(concat('upei50.behaviors.js'))  // concat and name it "concat.js"
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+//====================
+// Image Optimization
+//===================
+
+gulp.task('images', function(){
+    gulp.src('./src/gfx/**/*')
+    //.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(imagemin([
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./dist/gfx'));
+});
+
+//==================
+// Default Gulp Task
+//=================
+
+gulp.task ('default',['browser-sync'], function(){
+    //watch sass folder and compile changes
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    //watch js folder and compile changes
+    gulp.watch('src/js/**/*.js', ['js']);
+    //watch image folder and optimize
+    gulp.watch('src/gfx/**/*', ['images']);
+});
+
+
